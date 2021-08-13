68d0feaac0bc2e4599480ac196ccf1fa874d0050
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-08-13 11:35:55 +0700

    Update intheofficefor.config.js

--- projects/intheofficefor.config.js
+++ projects/intheofficefor.config.js
@@ -4,5 +4,15 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
 
     await page.goto('https://in-the-office-for.web.app/')
     await capture(page, 'home')
+    await page.goto('https://in-the-office-for.web.app/us.html')
+    await capture(page, 'us')
+    await page.goto('https://in-the-office-for.web.app/canada.html')
+    await capture(page, 'cannada')
+    await page.goto('https://in-the-office-for.web.app/england.html')
+    await capture(page, 'uk')
+    await page.goto('https://in-the-office-for.web.app/japan.html')
+    await capture(page, 'jp')
+    await page.goto('https://in-the-office-for.web.app/korea.html')
+    await capture(page, 'kr')
   },
 })
