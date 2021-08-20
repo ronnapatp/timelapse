624ce05a73ba2fcd7cfd27d59cbc689d2de7c6d8
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-08-20 14:23:00 +0700

    Create blog.config.js

---
+++ projects/blog.config.js
@@ -0,0 +1,9 @@
+  
+module.exports = /** @type {TimelapseProjectConfig} */ ({
+  async run({ page, capture }) {
+    await page.setViewport({ width: 1280, height: 720 })
+
+    await page.goto('https://ronnapatpblog.netlify.app/')
+    await capture(page, 'home')
+  },
+})
