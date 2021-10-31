1928e463897b837451e0bbebff447d2352327ddf
Ronnapat Srivoravilai <84321166+ronnapatp@users.noreply.github.com>
2021-10-31 12:49:20 +0700

    Update ronnapatblog.config.js

--- projects/ronnapatblog.config.js
+++ projects/ronnapatblog.config.js
@@ -2,7 +2,7 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
 
-    await page.goto('https://blog.ronnapatp.com/')
+    await page.goto('https://blog.ronnapat.com/')
     await capture(page, 'home')
   },
 })
