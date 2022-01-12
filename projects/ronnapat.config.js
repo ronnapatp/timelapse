024201bbdfe32afde946b9924003e8fb83e7a29a
Ronnapat Srivoravilai <84321166+ronnapatp@users.noreply.github.com>
2022-01-12 21:13:17 +0700

    Update ronnapat.config.js

--- projects/ronnapat.config.js
+++ projects/ronnapat.config.js
@@ -1,6 +1,6 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
-    await page.setViewport({ width: 1280, height: 720 })
+    await page.setViewport({ width: 1280, height: 2000 })
 
     await page.goto('https://www.ronnapat.com/')
     await capture(page, 'home')
