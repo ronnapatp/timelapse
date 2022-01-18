f3e054257f1686f0bc427cc96463f0fbdcf260d5
Ronnapat Srivoravilai <84321166+ronnapatp@users.noreply.github.com>
2022-01-18 14:12:45 +0700

    Delete ronnapat.config.js

--- projects/ronnapat.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = /** @type {TimelapseProjectConfig} */ ({
-  async run({ page, capture }) {
-    await page.setViewport({ width: 1280, height: 2000 })
-
-    await page.goto('https://www.ronnapat.com/')
-    await capture(page, 'home')
-  },
-})
