620e10c808ff84b6ec6e60683434482d1787bf56
Ronnapat Srivoravilai <84321166+ronnapatp@users.noreply.github.com>
2022-03-29 22:12:55 +0700

    Update intheofficefor.config.js

--- projects/intheofficefor.config.js
+++ projects/intheofficefor.config.js
@@ -1,19 +1,6 @@
 module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
-
-    await page.goto('https://in-the-office-for.web.app/')
-    await capture(page, 'home')
-    await page.goto('https://in-the-office-for.web.app/us.html')
-    await capture(page, 'us')
-    await page.goto('https://in-the-office-for.web.app/canada.html')
-    await capture(page, 'cannada')
-    await page.goto('https://in-the-office-for.web.app/england.html')
-    await capture(page, 'uk')
-    await page.goto('https://in-the-office-for.web.app/japan.html')
-    await capture(page, 'jp')
-    await page.goto('https://in-the-office-for.web.app/korea.html')
-    await capture(page, 'kr')
     await page.goto('https://operation-rome.bossoq.live/')
     await capture(page, 'kr')
   },
